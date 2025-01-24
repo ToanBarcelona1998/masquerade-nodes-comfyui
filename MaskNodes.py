1f45ccc005277eb3903dca7dd1163b36bc337928
Nguyen Van Toan <toannv@myserver.local>
2025-01-24 11:34:13 +0700

    Update MaskNodes.py

--- MaskNodes.py
+++ MaskNodes.py
@@ -773,6 +773,107 @@ class CutByMask:
             return (tensor2rgb(result),)
         else:
             return (result,)
+        
+
+class MultiCutByMask:
+    """
+    Cuts the images to the bounding box of the mask. If force_resize_width or force_resize_height are provided,
+    the image will be resized to those dimensions. The `mask_mapping_optional` input can be provided from a 
+    'Separate Mask Components' node to cut multiple pieces out of a single image in a batch.
+    """
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "mask": ("IMAGE",),
+                "force_resize_width": ("INT", {"default": 0, "min": 0, "max": VERY_BIG_SIZE, "step": 1}),
+                "force_resize_height": ("INT", {"default": 0, "min": 0, "max": VERY_BIG_SIZE, "step": 1}),
+            },
+            "optional": {
+                "mask_mapping_optional": ("MASK_MAPPING",),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "cut"
+
+    CATEGORY = "Masquerade Nodes"
+
+    def cut(self, image, mask, force_resize_width, force_resize_height, mask_mapping_optional=None):
+        if len(image.shape) < 4:
+            C = 1
+        else:
+            C = image.shape[3]
+
+        # We operate on RGBA to keep the code clean and then convert back after
+        image = tensor2rgba(image)
+        mask = tensor2mask(mask)
+
+        if mask_mapping_optional is not None:
+            image = image[mask_mapping_optional]
+
+        # Scale the mask to be a matching size if it isn't
+        B, H, W, _ = image.shape
+        mask = torch.nn.functional.interpolate(mask.unsqueeze(1), size=(H, W), mode='nearest')[:, 0, :, :]
+        MB, _, _ = mask.shape
+
+        if MB < B:
+            assert(B % MB == 0)
+            mask = mask.repeat(B // MB, 1, 1)
+
+        # masks_to_boxes errors if the tensor is all zeros, so we'll add a single pixel and zero it out at the end
+        is_empty = ~torch.gt(torch.max(torch.reshape(mask, [MB, H * W]), dim=1).values, 0.)
+        mask[is_empty, 0, 0] = 1.
+        boxes = masks_to_boxes(mask)
+        mask[is_empty, 0, 0] = 0.
+
+        min_x = boxes[:, 0]
+        min_y = boxes[:, 1]
+        max_x = boxes[:, 2]
+        max_y = boxes[:, 3]
+
+        width = max_x - min_x + 1
+        height = max_y - min_y + 1
+
+        use_width = int(torch.max(width).item())
+        use_height = int(torch.max(height).item())
+
+        if force_resize_width > 0:
+            use_width = force_resize_width
+
+        if force_resize_height > 0:
+            use_height = force_resize_height
+
+        alpha_mask = torch.ones((B, H, W, 4))
+        alpha_mask[:, :, :, 3] = mask
+
+        image = image * alpha_mask
+
+        images = []
+        for i in range(0, B):
+            if not is_empty[i]:
+                ymin = int(min_y[i].item())
+                ymax = int(max_y[i].item())
+                xmin = int(min_x[i].item())
+                xmax = int(max_x[i].item())
+                single = (image[i, ymin:ymax+1, xmin:xmax+1, :]).unsqueeze(0)
+                resized = torch.nn.functional.interpolate(single.permute(0, 3, 1, 2), size=(use_height, use_width), mode='bicubic').permute(0, 2, 3, 1)
+                images.append(resized[0])
+
+        # Stack the processed images back into a batch tensor
+        result = torch.stack(images, dim=0)
+
+        # Preserve our type unless we were previously RGB and added non-opaque alpha due to the mask size
+        if C == 1:
+            return (tensor2mask(result),)
+        elif C == 3 and torch.min(result[:, :, :, 3]) == 1:
+            return (tensor2rgb(result),)
+        else:
+            return (result,)
 
 class SeparateMaskComponents:
     """
@@ -932,6 +1033,150 @@ class PasteByMask:
                 resized_image = image_to_paste[i].unsqueeze(0)
                 if SH != height or SW != width:
                     resized_image = torch.nn.functional.interpolate(resized_image.permute(0, 3, 1, 2), size=(height,width), mode='bicubic').permute(0, 2, 3, 1)
+
+                pasting = torch.ones([H, W, C])
+                ymid = float(mid_y[i].item())
+                ymin = int(math.floor(ymid - height / 2)) + 1
+                ymax = int(math.floor(ymid + height / 2)) + 1
+                xmid = float(mid_x[i].item())
+                xmin = int(math.floor(xmid - width / 2)) + 1
+                xmax = int(math.floor(xmid + width / 2)) + 1
+
+                _, source_ymax, source_xmax, _ = resized_image.shape
+                source_ymin, source_xmin = 0, 0
+
+                if xmin < 0:
+                    source_xmin = abs(xmin)
+                    xmin = 0
+                if ymin < 0:
+                    source_ymin = abs(ymin)
+                    ymin = 0
+                if xmax > W:
+                    source_xmax -= (xmax - W)
+                    xmax = W
+                if ymax > H:
+                    source_ymax -= (ymax - H)
+                    ymax = H
+
+                pasting[ymin:ymax, xmin:xmax, :] = resized_image[0, source_ymin:source_ymax, source_xmin:source_xmax, :]
+                pasting[:, :, 3] = 1.
+
+                pasting_alpha = torch.zeros([H, W])
+                pasting_alpha[ymin:ymax, xmin:xmax] = resized_image[0, source_ymin:source_ymax, source_xmin:source_xmax, 3]
+
+                if resize_behavior == "keep_ratio_fill" or resize_behavior == "source_size_unmasked":
+                    # If we explicitly want to fill the area, we are ok with extending outside
+                    paste_mask = pasting_alpha.unsqueeze(2).repeat(1, 1, 4)
+                else:
+                    paste_mask = torch.min(pasting_alpha, mask[i]).unsqueeze(2).repeat(1, 1, 4)
+                result[image_index] = pasting * paste_mask + result[image_index] * (1. - paste_mask)
+        return (result,)
+    
+class MutilPasteByMask:
+    """
+    Pastes `image_to_paste` onto `image_base` using `mask` to determine the location. The `resize_behavior` parameter determines how the image to paste is resized to fit the mask. If `mask_mapping_optional` obtained from a 'Separate Mask Components' node is used, it will control which image gets pasted onto which base image.
+    """
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image_base": ("IMAGE",),
+                "image_to_paste": ("IMAGE",),  # this will be a list of images from MultiCutByMask
+                "mask": ("IMAGE",),
+                "resize_behavior": (["resize", "keep_ratio_fill", "keep_ratio_fit", "source_size", "source_size_unmasked"],)
+            },
+            "optional": {
+                "mask_mapping_optional": ("MASK_MAPPING",),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "paste"
+
+    CATEGORY = "Masquerade Nodes"
+
+    def paste(self, image_base, image_to_paste, mask, resize_behavior, mask_mapping_optional=None):
+        image_base = tensor2rgba(image_base)
+        image_to_paste = [tensor2rgba(img) for img in image_to_paste]  # Convert list of images
+        mask = tensor2mask(mask)
+
+        # Scale the mask to be a matching size if it isn't
+        B, H, W, C = image_base.shape
+        MB = mask.shape[0]
+        PB = len(image_to_paste)  # Number of images to paste
+
+        if mask_mapping_optional is None:
+            if B < PB:
+                assert(PB % B == 0)
+                image_base = image_base.repeat(PB // B, 1, 1, 1)
+            B, H, W, C = image_base.shape
+            if MB < B:
+                assert(B % MB == 0)
+                mask = mask.repeat(B // MB, 1, 1)
+            elif B < MB:
+                assert(MB % B == 0)
+                image_base = image_base.repeat(MB // B, 1, 1, 1)
+            if PB < B:
+                assert(B % PB == 0)
+                image_to_paste = image_to_paste * (B // PB)  # Repeat the list to match batch size
+        mask = torch.nn.functional.interpolate(mask.unsqueeze(1), size=(H, W), mode='nearest')[:, 0, :, :]
+        MB, MH, MW = mask.shape
+
+        # masks_to_boxes errors if the tensor is all zeros, so we'll add a single pixel and zero it out at the end
+        is_empty = ~torch.gt(torch.max(torch.reshape(mask, [MB, MH * MW]), dim=1).values, 0.)
+        mask[is_empty, 0, 0] = 1.
+        boxes = masks_to_boxes(mask)
+        mask[is_empty, 0, 0] = 0.
+
+        min_x = boxes[:, 0]
+        min_y = boxes[:, 1]
+        max_x = boxes[:, 2]
+        max_y = boxes[:, 3]
+        mid_x = (min_x + max_x) / 2
+        mid_y = (min_y + max_y) / 2
+
+        target_width = max_x - min_x + 1
+        target_height = max_y - min_y + 1
+
+        result = image_base.detach().clone()
+        for i in range(0, MB):
+            if is_empty[i]:
+                continue
+            else:
+                image_index = i
+                if mask_mapping_optional is not None:
+                    image_index = mask_mapping_optional[i].item()
+                source_size = image_to_paste[image_index].size()  # Get the corresponding image to paste
+                SB, SH, SW, _ = image_to_paste[image_index].shape
+
+                # Figure out the desired size
+                width = int(target_width[i].item())
+                height = int(target_height[i].item())
+                if resize_behavior == "keep_ratio_fill":
+                    target_ratio = width / height
+                    actual_ratio = SW / SH
+                    if actual_ratio > target_ratio:
+                        width = int(height * actual_ratio)
+                    elif actual_ratio < target_ratio:
+                        height = int(width / actual_ratio)
+                elif resize_behavior == "keep_ratio_fit":
+                    target_ratio = width / height
+                    actual_ratio = SW / SH
+                    if actual_ratio > target_ratio:
+                        height = int(width / actual_ratio)
+                    elif actual_ratio < target_ratio:
+                        width = int(height * actual_ratio)
+                elif resize_behavior == "source_size" or resize_behavior == "source_size_unmasked":
+                    width = SW
+                    height = SH
+
+                # Resize the image we're pasting if needed
+                resized_image = image_to_paste[image_index].unsqueeze(0)
+                if SH != height or SW != width:
+                    resized_image = torch.nn.functional.interpolate(resized_image.permute(0, 3, 1, 2), size=(height, width), mode='bicubic').permute(0, 2, 3, 1)
 
                 pasting = torch.ones([H, W, C])
                 ymid = float(mid_y[i].item())
@@ -1332,7 +1577,9 @@ NODE_CLASS_MAPPINGS = {
     "Mix Color By Mask": MixColorByMask,
     "Mask To Region": MaskToRegion,
     "Cut By Mask": CutByMask,
+    "MultiCut By Mask": MultiCutByMask,
     "Paste By Mask": PasteByMask,
+    "MultiPaste By Mask": MutilPasteByMask,
     "Get Image Size": GetImageSize,
     "Change Channel Count": ChangeChannelCount,
     "Constant Mask": ConstantMask,
@@ -1357,7 +1604,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Mix Color By Mask": "Mix Color By Mask",
     "Mask To Region": "Mask To Region",
     "Cut By Mask": "Cut By Mask",
+    "MultiCut By Mask": "MultiCut By Mask",
     "Paste By Mask": "Paste By Mask",
+    "MultiPaste By Mask": "MultiPaste By Mask",
     "Get Image Size": "Get Image Size",
     "Change Channel Count": "Change Channel Count",
     "Constant Mask": "Constant Mask",
